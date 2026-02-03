c565eb9d8b38b470167e4b72857f54c90f8e7cf7
sabiHakim <119191252+sabiHakim@users.noreply.github.com>
2026-02-03 12:23:06 +0300

    Navbar

--- centre-commercial/src/app/app.config.ts
+++ centre-commercial/src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+  // app.config.ts
+  import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+  import { provideRouter } from '@angular/router';
+  import { provideClientHydration } from '@angular/platform-browser';
+  import { routes } from './app.routes';
+  import { LucideAngularModule, Store } from 'lucide-angular';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
-};
+  export const appConfig: ApplicationConfig = {
+    providers: [
+      provideRouter(routes),
+      provideClientHydration(),
+      importProvidersFrom(
+        LucideAngularModule.pick({
+          Store,
+        })
+      )
+    ]
+  };
