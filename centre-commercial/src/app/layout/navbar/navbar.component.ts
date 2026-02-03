667e66855ee76aee3dd0b3278dffb15df239d404
sabiHakim <119191252+sabiHakim@users.noreply.github.com>
2026-02-03 13:13:01 +0300

    NavBar avec icone

--- centre-commercial/src/app/layout/navbar/navbar.component.ts
+++ centre-commercial/src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Store } from 'lucide-angular';
+import { LucideAngularModule } from 'lucide-angular';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  protected readonly StoreIcon = Store;
 }
